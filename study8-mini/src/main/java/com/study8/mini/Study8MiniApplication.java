5a23d7cc47984da464c3c0ec885c75b6de7f8782
HuyNH <huynh@unit.com.vn>
2024-11-19 11:15:50 +0700

    [HuyNH] Config Camunda 7

--- study8-mini/src/main/java/com/study8/mini/Study8MiniApplication.java
+++ study8-mini/src/main/java/com/study8/mini/Study8MiniApplication.java
@@ -1,9 +1,11 @@
 package com.study8.mini;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Study8MiniApplication {
 
 	public static void main(String[] args) {
