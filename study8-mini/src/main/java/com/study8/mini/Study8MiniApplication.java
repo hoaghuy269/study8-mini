e5de71930254b9138789ca0de2efcec60de0c8be
HuyNH <huynh@unit.com.vn>
2024-11-19 09:49:40 +0700

    [HuyNH] Init project

---
+++ study8-mini/src/main/java/com/study8/mini/Study8MiniApplication.java
@@ -0,0 +1,13 @@
+package com.study8.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study8MiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study8MiniApplication.class, args);
+	}
+
+}
