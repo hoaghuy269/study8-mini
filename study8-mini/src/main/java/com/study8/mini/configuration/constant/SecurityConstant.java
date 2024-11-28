8d7884eacd7046374cfb85dcd0a9a83f8027166c
HuyNH <huynh@unit.com.vn>
2024-11-28 13:36:11 +0700

    [HuyNH] Fix SonarQube

--- study8-mini/src/main/java/com/study8/mini/configuration/constant/SecurityConstant.java
+++ study8-mini/src/main/java/com/study8/mini/configuration/constant/SecurityConstant.java
@@ -1,6 +1,5 @@
 package com.study8.mini.configuration.constant;
 
-import com.study8.mini.common.constant.CommonApiConstant;
 import com.study8.mini.rest.constant.ApiConstant;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
