500f81f3af40c90db2545f06f95d39152875f04f
HuyNH <huynh@unit.com.vn>
2024-11-29 16:52:37 +0700

    [HuyNH] Add study8-camunda module project

---
+++ study8-camunda/src/main/java/com/study8/camunda/Study8CamundaApplication.java
@@ -0,0 +1,13 @@
+package com.study8.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study8CamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study8CamundaApplication.class, args);
+	}
+
+}
---
+++ study8-camunda/src/test/java/com/study8/camunda/Study8CamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study8.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study8CamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
